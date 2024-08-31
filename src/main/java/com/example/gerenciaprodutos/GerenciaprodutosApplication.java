e037bfe6cd2ead0999ca84bcff92dd1b0b186ae1
Rodrigo Menna <rmenna.barroso@gmail.com>
2024-08-31 07:30:03 -0400

    SpringBootapllication

---
+++ src/main/java/com/example/gerenciaprodutos/GerenciaprodutosApplication.java
@@ -0,0 +1,13 @@
+package com.example.gerenciaprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciaprodutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciaprodutosApplication.class, args);
+	}
+
+}
