6fcda779eba8a681f24a273d280152e36fcc0828
Rodrigo Menna <rmenna.barroso@gmail.com>
2024-08-31 16:51:36 -0400

    arquivo de inicialiazção de tests

---
+++ src/test/java/com/example/gerenciaprodutos/GerenciaprodutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerenciaprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciaprodutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
